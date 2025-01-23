998599c0ebb0bd466b75f0fdd8c315c39032e9b5
hanskiecg <155513903+hanskiecg@users.noreply.github.com>
2025-01-23 11:50:56 +0800

    code updates

---
+++ assets/js/scripts.js
@@ -0,0 +1,24 @@
+function loadContent(section) {
+    const sections = document.querySelectorAll('.home-content, .about-content, .playlist-content, .gallery-content');
+    sections.forEach(el => el.style.display = 'none');
+
+    const activeSection = document.getElementById(section);
+    if (activeSection) {
+        activeSection.style.display = 'block';
+    }
+
+    const links = document.querySelectorAll('.nav-link');
+    links.forEach(link => link.classList.remove('active'));
+
+    const activeLink = document.querySelector(`a[onclick="loadContent('${section}')"]`);
+    if (activeLink) {
+        activeLink.classList.add('active');
+    }
+
+    const navbarToggler = document.querySelector('.navbar-toggler');
+    const navbarCollapse = document.querySelector('.navbar-collapse');
+    if (navbarCollapse.classList.contains('show')) {
+        navbarToggler.click();
+    }
+}
+window.onload = () => loadContent('home');

4513ff9627c22c7f1ed3eae2aff2058af9745f4b
hanskiecg <155513903+hanskiecg@users.noreply.github.com>
2025-01-23 13:47:56 +0800

    updates

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -21,4 +21,27 @@ function loadContent(section) {
         navbarToggler.click();
     }
 }
+
+function showModal(name) {
+    const modalTitle = document.querySelector('#myModal .modal-title');
+    const modalBody = document.querySelector('#myModal .modal-body');
+
+    modalTitle.innerHTML = `${name}'s Personal Information`;
+    modalBody.innerHTML = 
+        `<p>
+            Hi beshy ${name}!<br><br>
+            SOBRANG LATINA! PUTANG INA SARAP TALAGA NG BABAE NA TO OH! HAYOP KA TALAGA! ANG GANDA GANDA MO SHET KA TALAGA PARANG UNANG TINGIN SAYO LALABASAN NA AGAD AKO SOBRANG GANDA MO OH! KA GWAPA VA UYTCHHHH! GRABE ANG PAGKA MONYEKA SA BABAE! MONYEKA! LATINA! GORJUICE! SOBRANG SARAP!
+        </p>`;
+
+    const myModal = new bootstrap.Modal(document.getElementById('myModal'));
+    myModal.show();
+
+    const links = document.querySelectorAll('.cs-name-link');
+    links.forEach(link => link.classList.remove('active'));
+
+    const activeLink = document.querySelector(`#link-${name}`);
+    if (activeLink) {
+        activeLink.classList.add('active');
+    }
+}
 window.onload = () => loadContent('home');
